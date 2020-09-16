7d2e2524e041ee484b0a3270b13c5ee7fb361188
roshaantahir <tahirroshan0@gmail.com>
2020-09-16 14:02:46 +0500

    map added

--- app/src/main/java/com/tvacstudio/tabs/MainActivity.java
+++ app/src/main/java/com/tvacstudio/tabs/MainActivity.java
@@ -4,11 +4,21 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
+
+import android.Manifest;
+import android.Manifest.permission;
+import android.app.Dialog;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
@@ -53,9 +63,39 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.getTabAt(2).setIcon(R.drawable.ic_baseline_update_24);
         tabLayout.getTabAt(3).setIcon(R.drawable.ic_baseline_delete_forever_24);
 
+        initGoogleMap();
 
 
+    }
 
+    private void initGoogleMap() {
+if (isServiceOk()){
+    if (checkLocationPermission()){
+        Toast.makeText(this, "Map ready", Toast.LENGTH_SHORT).show();
+    }else {
+        requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+    }
+}
+
+    }
+    private boolean isServiceOk(){
+        GoogleApiAvailability googleApi=GoogleApiAvailability.getInstance();
+        int result= googleApi.isGooglePlayServicesAvailable(this);
+        if (result == ConnectionResult.SUCCESS){
+            return true;
+        }else if (googleApi.isUserResolvableError(result)){
+            int PLAY_SERVICES_ERROR_CODE = 9002;
+            Dialog dialog = googleApi.getErrorDialog(this,result, PLAY_SERVICES_ERROR_CODE);
+            dialog.show();
+        }else {
+            Toast.makeText(this, "Play Services are required for this app", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
+    private boolean checkLocationPermission() {
+        return ContextCompat.checkSelfPermission(this, permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED;
     }
 
     private static class ViewPagerAdapter extends FragmentPagerAdapter {
